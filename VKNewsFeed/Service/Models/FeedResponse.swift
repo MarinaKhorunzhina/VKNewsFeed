c85348c2da3c79796a35e9bcfbe04952de3c5f95
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-07-20 21:10:09 +0300

    Implementing Parsing Logic

---
+++ VKNewsFeed/Service/Models/FeedResponse.swift
@@ -0,0 +1,30 @@
+//
+//  FeedResponse.swift
+//  VKNewsFeed
+//
+//  Created by Marina on 20.07.22.
+//
+
+import Foundation
+struct FeedResponseWrapped: Decodable {
+    let response: FeedResponse
+}
+
+struct FeedResponse: Decodable {
+    var items: [FeedItem]
+}
+
+struct FeedItem: Decodable {
+    let sourceId: Int
+    let postId: Int
+    let text: String?
+    let date: Double
+    let comments: CountableItem?
+    let likes: CountableItem?
+    let reposts: CountableItem?
+    let views: CountableItem?
+}
+
+struct CountableItem: Decodable {
+    let count: Int
+}
